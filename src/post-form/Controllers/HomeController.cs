90ac9181f1669804be70f24f6db1366acf6bf50e
PetrovaAnastasiya <33149053+PetrovaAnastasiya@users.noreply.github.com>
2020-02-25 23:52:07 +0300

    added

---
+++ src/post-form/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using post_form.Models;
+using BackendApi;
+using Grpc.Net.Client;
+
+
+namespace post_form.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> GetTaskId(string description)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Job.JobClient(channel);
+            var reply = await client.RegisterAsync(
+                              new RegisterRequest { Description = description });
+            ViewData["Message"] = "Task id = " + reply.Id + " Description = " + description;
+            return Ok("Task id = " + reply.Id + " Description = " + description);
+        }
+    }
+}
